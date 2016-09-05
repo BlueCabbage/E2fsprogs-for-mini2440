8a7eeba88f54b5f49caca1141e7b91210114bde7
BlueCabbage <479680168@qq.com>
2016-09-05 04:03:54 -0700

    for mini2440

---
+++ MINI2440_build/lib/dirpaths.h
@@ -0,0 +1,10 @@
+/*
+ * This file contains the path names for various directories as
+ * controlled by the configure script.
+ */
+
+/* Where to put the messages file for internationalization support */
+#define LOCALEDIR		"/home/zhaochao/Desktop/E2fsprogs/e2fsprogs/MINI2440_build/libs/share/locale"
+
+/* Where to find the mke2fs.conf and e2fsck.conf files */
+#define ROOT_SYSCONFDIR		"/home/zhaochao/Desktop/E2fsprogs/e2fsprogs/MINI2440_build/libs/etc"
